45042bdb3a8c3f646fb6e6b09a7bbdf51d14e9f4
Manas Pratim Biwas <manaspb2405@gmail.com>
2024-04-02 19:21:15 +0530

    [chore]: message components

--- hbd/src/App.tsx
+++ hbd/src/App.tsx
@@ -4,6 +4,7 @@ import polatoka from "/polatoka.mp3";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Audio } from "./components/Audio";
 import { Carousal } from "./components/Carousal";
+// import { Messages } from "./components/Messages";
 
 function App() {
   const [play] = useSound(polatoka, { loop: true });
@@ -16,6 +17,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Audio play={play} />} />
+          {/* <Route path="/birthday" element={<Messages />} /> */}
           <Route path="/wish" element={<Carousal />} />
         </Routes>
       </BrowserRouter>
---
+++ hbd/src/components/Balloons.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+
+export const Balloons:FC = ()=>{
+    return (
+        <>
+        <img src="/ballon2.svg" alt=""/>
+        <img src="/ballon1.svg" alt=""/>
+        <img src="/ballon3.svg" alt=""/>
+        <img src="/ballon1.svg" alt=""/>
+        <img src="/ballon2.svg" alt=""/>
+        <img src="/ballon3.svg" alt=""/>
+        <img src="/ballon2.svg" alt=""/>
+        <img src="/ballon3.svg" alt=""/>
+        <img src="/ballon1.svg" alt=""/>
+        <img src="/ballon2.svg" alt=""/>
+        <img src="/ballon3.svg" alt=""/>
+        <img src="/ballon2.svg" alt=""/>
+        <img src="/ballon1.svg" alt=""/>
+        <img src="/ballon3.svg" alt=""/>
+        <img src="/ballon2.svg" alt=""/>
+        <img src="/ballon3.svg" alt=""/>
+        <img src="/ballon1.svg" alt=""/>
+        <img src="/ballon2.svg" alt=""/>
+        <img src="/ballon1.svg" alt=""/>
+        <img src="/ballon3.svg" alt=""/>
+        <img src="/ballon3.svg" alt=""/>
+        <img src="/ballon1.svg" alt=""/>
+        <img src="/ballon2.svg" alt=""/>
+        <img src="/ballon3.svg" alt=""/>
+        <img src="/ballon2.svg" alt=""/>
+        <img src="/ballon1.svg" alt=""/>
+        <img src="/ballon3.svg" alt=""/>
+        <img src="/ballon2.svg" alt=""/>
+        <img src="/ballon3.svg" alt=""/>
+        <img src="/ballon1.svg" alt=""/>
+        <img src="/ballon2.svg" alt=""/>
+        <img src="/ballon1.svg" alt=""/>
+        <img src="/ballon3.svg" alt=""/>
+        </>
+    )
+}
