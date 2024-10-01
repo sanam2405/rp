e590367ab265de592084b908542c58f1010b10da
Manas Pratim Biswas <manaspb2405@gmail.com>
2024-10-01 23:49:46 +0530

    [chore]: update deps

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
     "jsx-a11y/click-events-have-key-events": ["off"],
     "jsx-a11y/no-noninteractive-element-interactions": ["off"],
     "jsx-a11y/media-has-caption": ["off"],
-
+    "@typescript-eslint/no-unnecessary-type-assertion": ["off"]
 
   },
 };
