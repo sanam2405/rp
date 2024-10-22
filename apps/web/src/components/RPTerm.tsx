0d04f570f4f518256cc0262bc0c86584dd6e307a
Manas Pratim Biswas <manaspb2405@gmail.com>
2024-10-22 14:10:20 +0530

    [feat]: sync ws/ui layer
    Signed-off-by: Manas Pratim Biswas <manaspb2405@gmail.com>

--- apps/web/src/components/RPTerm.tsx
+++ apps/web/src/components/RPTerm.tsx
@@ -6,6 +6,7 @@ import "@xterm/xterm/css/xterm.css";
 
 export const RPTerminal = () => {
   const terminalRef = useRef<HTMLDivElement | null>(null);
+  const ws = new WebSocket("ws://localhost:5005");
 
   useEffect(() => {
     const initTerminal = async () => {
@@ -20,9 +21,31 @@ export const RPTerminal = () => {
       term.open(terminalRef.current);
       fitAddon.fit();
       term.write("\x1B[1;3;31mrimjhim@sanam\x1B[0m $ 1 4 3 ");
+
+      // handles WebSocket messages
+      ws.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        if (data.type === "data") {
+          term.write(data.data);
+        }
+      };
+
+      // handles terminal key events
+      term.onKey((e) => {
+        ws.send(
+          JSON.stringify({
+            type: "command",
+            data: e.key,
+          }),
+        );
+      });
     };
 
     initTerminal();
+
+    return () => {
+      ws.close();
+    };
   }, []);
 
   return <div ref={terminalRef} className="w-full h-full p-0 m-0"></div>;
