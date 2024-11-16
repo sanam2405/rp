e327e9e2e58ab679e2a0f46f3062ebb9810087ce
Manas Pratim Biswas <manaspb2405@gmail.com>
2024-11-17 00:57:42 +0530

    [feat]: posthog capture redit events
    Signed-off-by: Manas Pratim Biswas <manaspb2405@gmail.com>

--- apps/web/src/components/RPMenuBar.tsx
+++ apps/web/src/components/RPMenuBar.tsx
@@ -21,36 +21,71 @@ import {
   TextTSlash,
 } from "@phosphor-icons/react/dist/ssr";
 import type { Editor } from "@tiptap/react";
+import { usePostHog } from "posthog-js/react";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export const RPMenuBar = ({ editor }: { editor: Editor }) => {
   const [showCopiedTooltip, setShowCopiedTooltip] = useState<boolean>(false);
-
+  const posthog = usePostHog();
+  const selection = editor.state.selection;
+  const selectedText = editor.state.doc.textBetween(
+    selection.from,
+    selection.to,
+  );
+  const currentEditorTextContent = editor.getText();
   const listOfIcons = [
     [
       {
         Icon: TextB,
         title: "Bold",
-        action: () => editor.chain().focus().toggleBold().run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "bold",
+            editor_content: currentEditorTextContent,
+            selected_text: selectedText,
+          });
+          editor.chain().focus().toggleBold().run();
+        },
         isActive: () => editor.isActive("bold"),
       },
       {
         Icon: TextItalic,
         title: "Italic",
-        action: () => editor.chain().focus().toggleItalic().run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "italic",
+            editor_content: currentEditorTextContent,
+            selected_text: selectedText,
+          });
+          editor.chain().focus().toggleItalic().run();
+        },
         isActive: () => editor.isActive("italic"),
       },
       {
         Icon: TextStrikethrough,
         title: "Strike",
-        action: () => editor.chain().focus().toggleStrike().run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "strike",
+            editor_content: currentEditorTextContent,
+            selected_text: selectedText,
+          });
+          editor.chain().focus().toggleStrike().run();
+        },
         isActive: () => editor.isActive("strike"),
       },
       {
         Icon: Highlighter,
         title: "Highlight",
-        action: () => editor.chain().focus().toggleHighlight().run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "highlight",
+            editor_content: currentEditorTextContent,
+            selected_text: selectedText,
+          });
+          editor.chain().focus().toggleHighlight().run();
+        },
         isActive: () => editor.isActive("highlight"),
       },
     ],
@@ -58,37 +93,73 @@ export const RPMenuBar = ({ editor }: { editor: Editor }) => {
       {
         Icon: TextHOne,
         title: "Heading 1",
-        action: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "heading_1",
+            editor_content: currentEditorTextContent,
+          });
+          editor.chain().focus().toggleHeading({ level: 1 }).run();
+        },
         isActive: () => editor.isActive("heading", { level: 1 }),
       },
       {
         Icon: TextHTwo,
         title: "Heading 2",
-        action: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "heading_2",
+            editor_content: currentEditorTextContent,
+          });
+          editor.chain().focus().toggleHeading({ level: 2 }).run();
+        },
         isActive: () => editor.isActive("heading", { level: 2 }),
       },
       {
         Icon: Paragraph,
         title: "Paragraph",
-        action: () => editor.chain().focus().setParagraph().run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "paragraph",
+            editor_content: currentEditorTextContent,
+          });
+          editor.chain().focus().setParagraph().run();
+        },
         isActive: () => editor.isActive("paragraph"),
       },
       {
         Icon: ListBullets,
         title: "Bullet List",
-        action: () => editor.chain().focus().toggleBulletList().run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "bullet_list",
+            editor_content: currentEditorTextContent,
+          });
+          editor.chain().focus().toggleBulletList().run();
+        },
         isActive: () => editor.isActive("bulletList"),
       },
       {
         Icon: ListNumbers,
         title: "Ordered List",
-        action: () => editor.chain().focus().toggleOrderedList().run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "ordered_list",
+            editor_content: currentEditorTextContent,
+          });
+          editor.chain().focus().toggleOrderedList().run();
+        },
         isActive: () => editor.isActive("orderedList"),
       },
       {
         Icon: ListChecks,
         title: "Task List",
-        action: () => editor.chain().focus().toggleTaskList().run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "task_list",
+            editor_content: currentEditorTextContent,
+          });
+          editor.chain().focus().toggleTaskList().run();
+        },
         isActive: () => editor.isActive("taskList"),
       },
     ],
@@ -96,28 +167,58 @@ export const RPMenuBar = ({ editor }: { editor: Editor }) => {
       {
         Icon: Quotes,
         title: "Blockquote",
-        action: () => editor.chain().focus().toggleBlockquote().run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "blockquote",
+            editor_content: currentEditorTextContent,
+          });
+          editor.chain().focus().toggleBlockquote().run();
+        },
         isActive: () => editor.isActive("blockquote"),
       },
       {
         Icon: Minus,
         title: "Horizontal Rule",
-        action: () => editor.chain().focus().setHorizontalRule().run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "horizontal_rule",
+            editor_content: currentEditorTextContent,
+          });
+          editor.chain().focus().setHorizontalRule().run();
+        },
       },
       {
         Icon: TextTSlash,
         title: "Clear Format",
-        action: () => editor.chain().focus().clearNodes().unsetAllMarks().run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "clear_format",
+            editor_content: currentEditorTextContent,
+          });
+          editor.chain().focus().clearNodes().unsetAllMarks().run();
+        },
       },
       {
         Icon: ArrowUUpLeft,
         title: "Undo",
-        action: () => editor.chain().focus().undo().run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "undo",
+            editor_content: currentEditorTextContent,
+          });
+          editor.chain().focus().undo().run();
+        },
       },
       {
         Icon: ArrowUUpRight,
         title: "Redo",
-        action: () => editor.chain().focus().redo().run(),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "redo",
+            editor_content: currentEditorTextContent,
+          });
+          editor.chain().focus().redo().run();
+        },
       },
     ],
     [
@@ -125,7 +226,11 @@ export const RPMenuBar = ({ editor }: { editor: Editor }) => {
         Icon: ClipboardText,
         title: showCopiedTooltip ? "Copied!" : "Copy Text",
         action: async () => {
-          await navigator.clipboard.writeText(editor.getText());
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "copy_text",
+            editor_content: currentEditorTextContent,
+          });
+          await navigator.clipboard.writeText(currentEditorTextContent);
           setShowCopiedTooltip(true);
           setTimeout(() => setShowCopiedTooltip(false), 2000);
         },
@@ -133,7 +238,13 @@ export const RPMenuBar = ({ editor }: { editor: Editor }) => {
       {
         Icon: ArrowsClockwise,
         title: "Reset",
-        action: () => editor.commands.setContent(""),
+        action: () => {
+          posthog.capture("redit.menubar_item", {
+            menubar_item: "reset",
+            editor_content: currentEditorTextContent,
+          });
+          editor.commands.setContent("");
+        },
       },
     ],
   ];
